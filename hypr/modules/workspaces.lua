d714d49ce25d3e759a4abc804b08c9358f9b064c
hbokhar2 <harrisbokhari@outlook.com>
2026-07-25 11:10:57 -0700

    Fixed animation issues in 0.56 hyprland.

--- hypr/modules/workspaces.lua
+++ hypr/modules/workspaces.lua
@@ -4,7 +4,7 @@ return {
 	hl.workspace_rule({ workspace = "2", monitor = "DP-4", persistent = true}),
 	hl.workspace_rule({ workspace = "3", monitor = "DP-4", persistent = true }),
 
-	hl.workspace_rule({ workspace = "4", monitor = "DP-3", default = true }),
+	hl.workspace_rule({ workspace = "4", monitor = "DP-3", default = true, persistent = true }),
 	hl.workspace_rule({ workspace = "5", monitor = "DP-3", persistent = true}),
 	hl.workspace_rule({ workspace = "6", monitor = "DP-3", persistent = true }),
 
